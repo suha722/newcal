694cc4ee9daee91f0bbfb2d352731feff1953c38
soha ardah <git@github.com>
2021-07-27 18:53:29 +0300

    load commit

---
+++ calculater/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculater
+{  string oper = "";
+     double result = 0;
+     bool isoper = false;
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Button but = (Button)sender;
+            oper = but.Text;
+            result = double.Parse(textBox1.Text);
+            isoper = true;
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Button but = (Button)sender;
+            if (textBox1.Text == "0"||isoper==true) { textBox1.Clear(); }
+            if (but.Text == ",") { 
+                if (!textBox1.Text.Contains(",")) { 
+                    textBox1.Text = textBox1.Text + but.Text; }}
+               else { textBox1.Text = textBox1.Text + but.Text;
+                
+            }isoper = false;
+         
+        
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            switch (oper)
+            {
+                case "+":
+                    textBox1.Text = (result + double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "-":
+                    textBox1.Text = (result - double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "*":
+                    textBox1.Text = (result * double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "/":
+                    textBox1.Text = (result / double.Parse(textBox1.Text)).ToString();
+                    break;
+
+
+
+            }
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            result = 0;
+        }
+    }
+}
